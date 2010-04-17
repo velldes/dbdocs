4265a0c903076ce111c457bc9fa5af063520fef0
velldesign <velldesign@e03a0cbc-2fa4-16de-8f4d-7b212c82725d>
2010-04-17 20:27:31 +0000

    Added Copyright information Added new screenshots

--- DbDoc/src/info/vstour/dbdoc/server/DocServiceImpl.java
+++ DbDoc/src/info/vstour/dbdoc/server/DocServiceImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 Roman Mishchenko
+ * 
+ * This file is part of DbDoc. Project web is http://code.google.com/p/dbdocs/
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 package info.vstour.dbdoc.server;
 
 import info.vstour.dbdoc.client.DocService;
@@ -126,7 +141,7 @@ public class DocServiceImpl extends RemoteServiceServlet implements DocService {
 		String[] props = null;
 		try {
 			URL dir = new URL(DbDocRes.get("").BASE_URL + DbDocRes.get("").PROPS_RES);
-			props = DbDocRes.get("").getList(new File(dir.toURI()), ".*");
+			props = DbDocRes.get("").getList(new File(dir.toURI()), ".*properties");
 		}
 		catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
