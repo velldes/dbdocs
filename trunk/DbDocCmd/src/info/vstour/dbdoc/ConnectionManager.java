43a1a55d1c752b25d9c2dc57c72786c2991560a6
velldesign <velldesign@e03a0cbc-2fa4-16de-8f4d-7b212c82725d>
2010-04-17 20:22:42 +0000

    Added Copyright information Documentation example moved to zip archive

--- trunk/DbDocCmd/src/info/vstour/dbdoc/ConnectionManager.java
+++ trunk/DbDocCmd/src/info/vstour/dbdoc/ConnectionManager.java
@@ -1,12 +1,27 @@
+/*
+ * Copyright 2010 Roman Mishchenko
+ * 
+ * This file is part of DbDoc. Project web is http://code.google.com/p/dbdocs/
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 package info.vstour.dbdoc;
+
+import info.vstour.dbdoc.shared.PropsConstants;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-/**
- * @author Roman Mishchenko
- */
 public class ConnectionManager {
 
 	private String	driver;
@@ -15,10 +30,10 @@ public class ConnectionManager {
 	private String	password;
 
 	public ConnectionManager(Properties props) {
-		driver = props.getProperty("Driver");
-		url = props.getProperty("Url");
-		user = props.getProperty("User");
-		password = props.getProperty("Password");
+		driver = props.getProperty(PropsConstants.DRIVER);
+		url = props.getProperty(PropsConstants.URL);
+		user = props.getProperty(PropsConstants.USER);
+		password = props.getProperty(PropsConstants.PASSWORD);
 	}
 
 	public Connection getConnection() throws ClassNotFoundException, SQLException {
